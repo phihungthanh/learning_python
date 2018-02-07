81188e59ff1d1a8032d9ea50d9495114addb9d84
phi <phihungthanh@gmail.com>
2018-02-07 12:19:56 -0800

    updated this morning session.

---
+++ chpt5_ATBS_dictionary/birthday_test.py
@@ -0,0 +1,18 @@
+__author__ = 'coiwaxa'
+""" a use case for dictionary"""
+birthdays = {'Alice': 'Apr 1', 'Bob':'Dec 12', 'Carol':'Mar 4'}
+while True:
+    print('Enter a name: (black to quit)')
+    name = input()
+    if name == '':
+        break
+
+    if name in birthdays:
+        print(birthdays[name]+ ' is the birthday of '+name)
+    else:
+        print('I do not have birthday information for' +name)
+        print('What is their birthday?')
+        bday = input()
+        birthdays[name]=bday
+        print('Birthday database updated.')
+
